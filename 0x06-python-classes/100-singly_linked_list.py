08a721519824741e7a396b171d31a68353f3187f
stephen_adah <mailstephenreal@gmail.com>
2023-09-27 01:15:27 -0700

    7. Singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,79 @@
+#!/us/bin/python3
+"""Write a class Node that defines a node of a singly linked list"""
+
+
+class Node:
+    """class Node that defines a node of a singly linked list"""
+
+    def __init__(self, data, next_node=None):
+        """Initialize a class Node..
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the class square.
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrive the current size of square"""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieve the position of the square"""
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """class of singly-linked list"""
+
+    def __init__(self):
+        """Initialize a class Singly-linked list"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the Singly-linked list.
+        In order of numerical position.
+
+        Args:
+            value (Node): The data value for the new Node to insert.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            current = self.__head
+            while (
+                    current.next_node is not None and
+                    current.next_node.data < value
+            ):
+                current = current.next_node
+            new.next_node = current.next_node
+            current.next_node = new
+
+    def __str__(self):
+        """a print function rep. of a singly Linked-list"""
+        values = []
+        current = self.__head
+        while current is not None:
+            values.append(str(current.data))
+            current = current.next_node
+        return ('\n'.join(values))
