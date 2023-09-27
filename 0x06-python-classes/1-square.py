bf6969e235ab9613e78f6ac00a8c7cc7d26516e5
stephen_adah <mailstephenreal@gmail.com>
2023-09-26 20:19:33 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a Square."""
+
+    def __init__(self, size):
+        """Initialize the class Square.
+
+
+        Args:
+            size (int): The size of the new Square.
+        """
+
+        self.__size = size
