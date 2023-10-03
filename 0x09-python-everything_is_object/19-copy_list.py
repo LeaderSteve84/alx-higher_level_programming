cde5eb9c093fd601c6a14723f70e84632423550a
stephen_adah <mailstephenreal@gmail.com>
2023-10-03 04:15:30 -0700

    19. Copy a list object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def copy_list(l):
+''' a function def copy_list(l): that returns a copy of a list.'''
+
+
+def copy_list(my_list):
+    '''function that returns a copy of a list.
+    Args:
+        my_list (list): A list as argument
+    '''
     return l.copy()
