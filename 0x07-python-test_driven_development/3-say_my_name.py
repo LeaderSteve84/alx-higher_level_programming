a17c352af9f725ec27117a5366424e6e1ba2804a
stephen_adah <mailstephenreal@gmail.com>
2023-10-05 11:43:25 -0700

    tests/3-say_my_name.txt

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,7 +14,7 @@ def say_my_name(first_name, last_name=""):
         TypeError: first_name and last_name must be strings
     '''
 
-    if not ininstance(first_name, str):
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
