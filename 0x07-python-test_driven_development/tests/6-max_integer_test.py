8affa8ce5f13ae312daad5b1c15e5593bc8a90de
stephen_adah <mailstephenreal@gmail.com>
2023-10-05 14:31:34 -0700

    tests/6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,7 +5,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class Test_MaxInteger(unittest.TestCase):
+class Test_Max_Integer(unittest.TestCase):
     '''define unittests for max_integer(list=[])'''
 
     def test_ordered_list(self):
@@ -13,22 +13,22 @@ class Test_MaxInteger(unittest.TestCase):
         ordered = [1, 2, 4, 3]
         self.assertEqual(max_integer(ordered), 4)
 
-    def test_maxAtBeginning(self):
+    def test_max_At_Beginning(self):
         '''Test a list with a beginning max value.'''
-        maxAtBeginning = [4, 3, 2, 1]
-        self.assertEqual(max_integer(maxAtBeginning), 4)
+        max_At_Beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_At_Beginning), 4)
 
-    def test_emptyList(self):
+    def test_empty_List(self):
         '''test an empty list.'''
         empty = []
         self.assertEqual(max_integer(empty), None)
 
-    def test_oneElementList(self):
+    def test_one_Element_List(self):
         '''test a list with asingle element'''
         one_element = [7]
         self.assertEqual(max_integer(one_element), 7)
 
-    def testFloats(self):
+    def test_Floats(self):
         '''test a list of floats.'''
         floats = [1.53, 6.33, -9.123, 15.2, 6.0]
         self.assertEqual(max_integer(floats), 15.2)
@@ -43,16 +43,15 @@ class Test_MaxInteger(unittest.TestCase):
         string = "bigail"
         self.assertEqual(max_integer(string), 'r')
     
-    def test_listOf_strings(self):
+    def test_list_Of_strings(self):
         '''test a list of strings.'''
         strings = ["bigail", "is", "my", "name"]
         self.assertEqual(max_integer(strings), "name")
 
-    def testEmptyString(self):
+    def test_Empty_String(self):
         '''test an empty string.'''
         self.assertEqual(max_integer(""), None)
 
   
 if __name__ == "__main__":
     unittest.main()
-
