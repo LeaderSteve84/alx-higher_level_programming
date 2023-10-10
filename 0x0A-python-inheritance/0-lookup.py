13606ac76adf68373129260aa46c022e8e9019bd
stephen_adah <mailstephenreal@gmail.com>
2023-10-09 23:14:37 -0700

    compliance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -13,4 +13,4 @@ def lookup(obj):
 
     Return: List of attributes and methods
     '''
-    return(dir(obj))
+    return (dir(obj))
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A module of a function that returns True if 
-    the object is an instance of, or if the object
-    is an instance of a class that inherited from,
-    the specified class ; otherwise False.
+"""A module of a function that returns True if
+the object is an instance of, or if the object
+is an instance of a class that inherited from,
+the specified class ; otherwise False.
 """
 
 
