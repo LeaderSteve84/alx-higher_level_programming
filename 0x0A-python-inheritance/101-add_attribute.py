fdfc998f319956c09846943206333391599672f3
stephen_adah <mailstephenreal@gmail.com>
2023-10-10 01:11:13 -0700

    13. Can I?

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module of a function that adds a new
+attribute to an object if it’s possible:
+"""
+
+
+def add_attribute(obj, attri, value):
+    """A method to add a new attribute.
+
+    Args:
+        obj (any): The object
+        att (str): The name of attribute
+        value (any): The value
+
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
