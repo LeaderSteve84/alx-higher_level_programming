d2349c104025fff248a66adfb8c9a0f4ff0ec3ae
stephen_adah <mailstephenreal@gmail.com>
2023-10-11 08:23:31 -0700

    14. Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""A module of a script that reads stdin
+line by line and computes metrics:
+"""
+
+
+def print_stats(size, status_codes):
+    """function that print  accumulated matrix.
+
+    Args:
+        size (int): The read file
+        status_codes (dict): The count of status code
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '300', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-1] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
