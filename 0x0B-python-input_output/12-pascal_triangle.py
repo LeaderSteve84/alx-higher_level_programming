86d77f6f96769a7e6981d1bc12ac7adb48f69f1c
stephen_adah <mailstephenreal@gmail.com>
2023-10-11 06:30:33 -0700

    12. Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A module of a pascal triangle function"""
+
+
+def pascal_triangle(n):
+    """A function pascal's triangle of
+    size n.
+
+    Return a list of lists of integers representating
+    the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
