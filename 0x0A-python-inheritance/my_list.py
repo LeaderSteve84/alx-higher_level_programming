9a1962f4b363a4752ed11b0f799fcd78e0fa13e7
stephen_adah <mailstephenreal@gmail.com>
2023-10-12 01:29:07 -0700

    1-my_list.py

--- 0x0A-python-inheritance/my_list.py
+++ 0x0A-python-inheritance/my_list.py
@@ -5,13 +5,7 @@
 class MyList(list):
     """ a class MyList that inherits from list
     """
-    def __init__(self, input_list=None):
-        if input_list is None:
-            super().__init__()
-        elif isinstance(input_list, list):
-            super().__init__(input_list)
-        else:
-            super().__init__([input_list])
+
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
