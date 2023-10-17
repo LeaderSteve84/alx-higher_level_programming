409083e353c46a9df54d509284b2dace114c5f07
stephen_adah <mailstephenreal@gmail.com>
2023-10-16 20:30:55 -0700

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,3 +94,64 @@ class Rectangle(Base):
             [print(" ", end="") for x in range(self.x)]
             [print("#", end="") for w in range(self.width)]
             print("")
+
+    def update(self, *args, **kwargs):
+        """Update the class Rectangle by overriding the __str__ method
+        so that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>.
+
+        Args:
+            *args (ints): various attribute values.
+                - argument 1st - id attribute
+                - argument 2nd - width attribute
+                - argument 3rd - height attribute
+                - argument 4th - x attribute
+                - argument 5th - y attribute
+            **kwargs (dict): key/value pair of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """return the dict rep of a rectangle"""
+        return {
+            "id": self.id,
+            "width": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """return the print() and str() rep of the rectangle"""
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.x, self.y, self.width, self.height))
