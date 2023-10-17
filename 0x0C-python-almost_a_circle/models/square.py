13d150d55d9edcb64fb26e2ce54d1092f7dca0fb
stephen_adah <mailstephenreal@gmail.com>
2023-10-16 23:04:50 -0700

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,20 +39,20 @@ class Square(Rectangle):
             **kwargs (dict): key/value pair of attributes.
         """
         if args and len(args) != 0:
-            a = 0
+            n = 0
             for arg in args:
-                if a == 0:
+                if n == 0:
                     if arg is None:
                         self.__init__(self.size, self.x, self.y)
                     else:
                         self.id = arg
-                elif a == 1:
+                elif n == 1:
                     self.size = arg
-                elif a == 2:
+                elif n == 2:
                     self.x = arg
-                elif a == 3:
+                elif n == 3:
                     self.y = arg
-                a += 1
+                n += 1
 
         elif kwargs and len(kwargs) != 0:
             for i, j in kwargs.items():
