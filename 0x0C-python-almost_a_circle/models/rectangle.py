fe1357fd3c4c4faf830fc3fb94e86b8ba1b00f1f
stephen_adah <mailstephenreal@gmail.com>
2023-10-16 23:00:29 -0700

    7. Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,9 +90,9 @@ class Rectangle(Base):
             return
 
         [print("") for y in range(self.y)]
-        for x in range(self.height):
-            [print(" ", end="") for y in range(self.x)]
-            [print("#", end="") for z in range(self.width)]
+        for h in range(self.height):
+            [print(" ", end="") for i in range(self.x)]
+            [print("#", end="") for j in range(self.width)]
             print("")
 
     def update(self, *args, **kwargs):
