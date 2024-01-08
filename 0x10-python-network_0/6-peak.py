ec1bbd4b814f849529be35f25f23238b7b255252
stephen_adah <mailstephenreal@gmail.com>
2024-01-08 12:09:49 +0100

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+"""finds a peak"""
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
