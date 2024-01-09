5257da8cfd82f782e8ee2f6a0ca3195177e0fcf2
stephen_adah <mailstephenreal@gmail.com>
2024-01-09 08:53:32 +0100

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as
+a parameter."""
+
+
+if __name__ == "__main__":
+    from requests import post
+    from sys import argv
+
+    URL = 'http://0.0.0.0:5000/search_user'
+    data = {'q': argv[1] if len(argv) >= 2 else ""}
+    res = post(URL, data)
+
+    res_type = res.headers['content-type']
+
+    if res_type == 'application/json':
+        result = res.json()
+        _d_id = result.get('id')
+        name = result.get('name')
+        if (result != {} and d_id and name):
+            print("[{}] {}".format(d_id, name))
+        else:
+            print('No result')
+    else:
+        print('Not a valid JSON')
