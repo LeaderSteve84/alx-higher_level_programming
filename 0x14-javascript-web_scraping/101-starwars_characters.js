28e3403afa6a123c5b9e653a0046544be03e08f3
stephen_adah <mailstephenreal@gmail.com>
2024-02-01 11:35:04 +0100

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie:
+
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
+
+req.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // print out character
+    for (const character of characters) {
+      req.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
+    }
+  }
+});
